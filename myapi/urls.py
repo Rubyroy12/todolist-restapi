4c155175440220c93e30e25cc64b18681dd75d1f
Rubyroy12 <you@example.com>
2021-07-16 01:26:14 +0300

    add bootstrap4

--- myapi/urls.py
+++ myapi/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path,include
+from . import views
+
+
+
+urlpatterns = [
+    path(r'^api/merch/$', views.HeroList.as_view()),
+    path('',views.home, name ='home')
+]
--- myapi/views.py
+++ myapi/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import HeroSerializer
+from .serializers import HeroSereializer
 from .models import Hero 
 # Create your views here
 
 class HeroList(APIView): #ModelViewSet  handles the GET and POST requests for our models
   def get(self, request,format=None):
       queryset = Hero.objects.all()
-      serializers= HeroSerializer(queryset, many=True)
+      serializers= HeroSereializer(queryset, many=True)
       return Response(serializers.data)
+
+def home(request):
+    return render(request,'home.html')
+
 
